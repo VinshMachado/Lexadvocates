c8936ffc674760228e812b1423667ef1810d094b
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-05-18 22:50:29 +0530

    .env

--- lexadvocates/app/Mycomponents/News.tsx
+++ lexadvocates/app/Mycomponents/News.tsx
@@ -5,7 +5,7 @@ const News = () => {
     <div className="w-full h-auto flex justify-center items-center  mt-10 mb-10">
       <iframe
         className="w-[90%] sm:h-[500px] h-[600px]"
-        src="https://rss.app/embed/v1/wall/IMDvqz9QgiMZs2hd"
+        src={process.env.MYNEWSKEY}
       ></iframe>
     </div>
   );
