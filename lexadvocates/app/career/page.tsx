0c550dec9f0cccd6ae95a27c3fc216ad7750ea97
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-05-23 22:24:01 +0530

    some fix

--- lexadvocates/app/career/page.tsx
+++ lexadvocates/app/career/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+
 import { Separator } from "@/components/ui/separator";
 import { motion } from "framer-motion";
 
