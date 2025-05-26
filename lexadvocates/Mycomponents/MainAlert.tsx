6e89a512e8ec2c86ab052afb67eae0931bd5f54f
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-05-26 15:00:50 +0530

    over flow fix

--- lexadvocates/Mycomponents/MainAlert.tsx
+++ lexadvocates/Mycomponents/MainAlert.tsx
@@ -21,12 +21,12 @@ const MainAlert = () => {
             <AlertDialogTitle>{"Disclaimer & Confirmation"}</AlertDialogTitle>
             <AlertDialogDescription>
               <h3 className="mb-5">{`As per the rules of the Bar Council of India, we are not permitted to solicit work and advertise. By clicking on the “I AGREE” button below, you acknowledge the following:`}</h3>
-              <h3>{`there has been no advertisement, personal communication, solicitation, invitation or inducement of any sort whatsoever from us or any of our members to solicit any work through this website;`}</h3>
-              <h3>{`you wish to gain more information about us for your own information and use;
-the information about us is provided to you on your specific request and any information obtained or materials downloaded from this website is completely at your own volition and any transmission, receipt or use of this site does not create any lawyer-client relationship; and that`}</h3>
-              <h3>{`we are not liable for any consequence of any action taken by you relying on the material / information provided on this website.`}</h3>
-              <h3>{`If you have any legal issues, you, in all cases, must seek independent legal advice.
-We use cookies to enhance your experience. By continuing to visit this website you agree to our use of cookies.`}</h3>
+              {`there has been no advertisement, personal communication, solicitation, invitation or inducement of any sort whatsoever from us or any of our members to solicit any work through this website;`}
+              {`you wish to gain more information about us for your own information and use;
+the information about us is provided to you on your specific request and any information obtained or materials downloaded from this website is completely at your own volition and any transmission, receipt or use of this site does not create any lawyer-client relationship; and that`}
+              {`we are not liable for any consequence of any action taken by you relying on the material / information provided on this website.`}
+              {`If you have any legal issues, you, in all cases, must seek independent legal advice.
+We use cookies to enhance your experience. By continuing to visit this website you agree to our use of cookies.`}
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
