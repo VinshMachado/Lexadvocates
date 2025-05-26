d434fe2733f9a9cf3755006da963a668e50dc677
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-05-26 15:16:18 +0530

    alert fix

--- lexadvocates/Mycomponents/MainAlert.tsx
+++ lexadvocates/Mycomponents/MainAlert.tsx
@@ -16,10 +16,10 @@ const MainAlert = () => {
   return (
     <>
       <AlertDialog open={isopen}>
-        <AlertDialogContent className="bg-white">
+        <AlertDialogContent className="bg-white h-[70%] overflow-auto">
           <AlertDialogHeader>
             <AlertDialogTitle>{"Disclaimer & Confirmation"}</AlertDialogTitle>
-            <AlertDialogDescription>
+            <AlertDialogDescription className="text-sm sm:text-md">
               <h3 className="mb-5">{`As per the rules of the Bar Council of India, we are not permitted to solicit work and advertise. By clicking on the “I AGREE” button below, you acknowledge the following:`}</h3>
               {`there has been no advertisement, personal communication, solicitation, invitation or inducement of any sort whatsoever from us or any of our members to solicit any work through this website;`}
               {`you wish to gain more information about us for your own information and use;
@@ -29,36 +29,39 @@ the information about us is provided to you on your specific request and any inf
 We use cookies to enhance your experience. By continuing to visit this website you agree to our use of cookies.`}
             </AlertDialogDescription>
           </AlertDialogHeader>
-          <AlertDialogFooter>
-            <label className="inline-flex items-center gap-2 cursor-pointer">
-              <input
-                type="checkbox"
-                checked={selection === true}
-                onChange={() => setSelection(true)}
-                className="form-checkbox text-blue-600 w-5 h-5"
-              />
-              <span>I Agree</span>
-            </label>
-
-            <label className="inline-flex items-center gap-2 cursor-pointer">
-              <input
-                type="checkbox"
-                checked={selection === false}
-                onChange={() => setSelection(false)}
-                className="form-checkbox text-red-600 w-5 h-5"
-              />
-              <span>I Disagree</span>
-            </label>
+          <AlertDialogFooter className="items-center justify-center items-center">
             <AlertDialogAction
               onClick={() => {
                 if (selection) {
                   setopen(false);
                 }
               }}
-              className="bg-amber-400"
+              className="bg-amber-400 mt-3 w-full sm:w-auto"
             >
               Continue
             </AlertDialogAction>
+            <div className="w-full flex justify-center items-center">
+              <label className="flex items-center gap-2 ">
+                <input
+                  type="checkbox"
+                  checked={selection === true}
+                  onChange={() => setSelection(true)}
+                  className="form-checkbox text-blue-600 w-5 h-5"
+                />
+                <span>I Agree</span>
+              </label>
+
+              <label className="inline-flex items-center gap-2 ml-5 cursor-pointer">
+                <input
+                  type="checkbox"
+                  checked={selection === false}
+                  onChange={() => setSelection(false)}
+                  className="form-checkbox text-red-600 w-5 h-5"
+                />
+
+                <span>I Disagree</span>
+              </label>
+            </div>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
