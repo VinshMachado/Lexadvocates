102cba3eac1b16f5598698df1c3be95a03306b26
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2025-06-07 00:28:25 +0530

    fixxes3

--- lexadvocates/app/Contact/page.tsx
+++ lexadvocates/app/Contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { Mail, Phone } from "lucide-react";
-import { motion, number } from "framer-motion";
+import { motion } from "framer-motion";
 import { MessageCircle } from "lucide-react";
 import { useState } from "react";
 export default function Page() {
